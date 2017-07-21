c8bbc2a6f50deac424cdab34d5c10d50f0de26a2
yusuf <slitgu@gmail.com>
2017-07-21 14:21:44 +0700

    near OK

--- app/src/main/java/rmutsv/alisa/yusuf/mynavigater/MainActivity.java
+++ app/src/main/java/rmutsv/alisa/yusuf/mynavigater/MainActivity.java
@@ -1,7 +1,10 @@
 package rmutsv.alisa.yusuf.mynavigater;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add Maps Controller
+        addMapsController();
+
+
+    } // main method
+
+    private void addMapsController() {
+        ImageView imageView = (ImageView) findViewById(R.id.imvAddMaps);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddMapsActivity.class));
+            }
+        });
     }
-}
+}  // main class
