8f0402a3f828c5ce49d11eb2318c12d06dc8b838
yusuf <slitgu@gmail.com>
2017-07-21 16:58:57 +0700

    finish 21 july

--- app/src/main/java/rmutsv/alisa/yusuf/mynavigater/AddMapsActivity.java
+++ app/src/main/java/rmutsv/alisa/yusuf/mynavigater/AddMapsActivity.java
@@ -1,7 +1,15 @@
 package rmutsv.alisa.yusuf.mynavigater;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,34 +21,131 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class AddMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private double latADouble = 7.087465, lngADouble = 100.245960;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        // Setup Constant
+        setupConstant();
+
+        // Create Maps Fragment
+        createMapsFragment();
+
+    }   // Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.removeUpdates(locationListener);
+
+        // For NetWork
+        Location netWork = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (netWork != null) {
+            latADouble = netWork.getLatitude();
+            lngADouble = netWork.getLongitude();
+        }
+
+        // For GPS
+        Location gps = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gps != null) {
+            latADouble = gps.getLatitude();
+            lngADouble = gps.getLongitude();
+        }
+
+        // Show Log
+        showLog();
+
+
+    }
+
+    private void showLog() {
+        String tag = "21julyV1";
+        Log.d(tag, "Lat ==>" + latADouble);
+        Log.d(tag, "Lnt ==>"+lngADouble);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+
+    }
+
+    public Location myFindLocation(String strProvider) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+        }
+
+        return location;
+    }
+
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            latADouble = location.getLatitude();
+            lngADouble = location.getLongitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private void setupConstant() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+    }
+
+    private void createMapsFragment() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+        // Create Center Map
+        LatLng latLng = new LatLng(latADouble, lngADouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+
+    }   // onMapReady
+}    // Main Class
